a712d3855445164cc5048cf4e7c5a65bd3be0dc0
ryanplusplus <ryanplusplus@gmail.com>
2014-04-07 21:56:31 -0400

    Gravity now applies to everything WRT everything.  Added the ability to create differently-sized bodies.  Added the ability to generate a bunch of random bodies.

--- main.lua
+++ main.lua
@@ -1,41 +1,100 @@
+GravitationConstant = 2
+VelocityFactor = 2.0
+RadiusFactor = 0.33
+DtLimit = 0.02
+
+CreationMass = 10
+Press = {}
+Bodies = {}
+
 function love.load()
-  GravitationConstant = 1500
-  VelocityFactor = 3.0
-
-  Press = {}
-  Bodies = {}
-
-  local width, height = love.window.getDimensions()
-
-  Sun = {
-    position = {x = width / 2, y = height / 2},
-    mass = 10000}
-
   love.graphics.setColor(0, 0, 0)
   love.graphics.setBackgroundColor(255, 255, 255)
   love.graphics.setLineStyle("smooth")
 
   love.window.setTitle("Orbital")
+  love.window.setFullscreen(true, "desktop")
+
+  math.randomseed(os.time())
+end
+
+function generateBodies()
+  local width, height = love.window.getDimensions()
+
+  for _ = 1, 250 do
+    local x = math.random(-width / 2, 1.5 * width)
+    local y = math.random(-height / 2, 1.5 * width)
+    local xvel = 0
+    local yvel = 0
+    local radius = 1
+    local mass = radiusToMass(radius)
+
+    Bodies[#Bodies + 1] = {
+      position = {x = x, y = y},
+      velocity = {x = xvel, y = yvel},
+      acceleration = {x = 0, y = 0},
+      mass = mass,
+      radius = radius}
+  end
+end
+
+function radiusToMass(radius)
+  return radius^3 * 4/3 * 3.14
+end
+
+function massToRadius(mass)
+  return (mass * 3/4 / 3.14)^(1/3)
 end
 
 function dist(x1, y1, x2, y2)
   return math.sqrt((x1 - x2)^2 + (y1 - y2)^2)
 end
 
-function updateAcceleration(body)
+function bodyDist(body1, body2)
+  return dist(body1.position.x, body1.position.y, body2.position.x, body2.position.y)
+end
+
+function colliding(body1, body2)
+  return bodyDist(body1, body2) < math.max (body1.radius, body2.radius)
+end
+
+function tooFarAway(body)
+  local width, height = love.window.getDimensions()
+
+  return body.position.x > 3 * width
+      or body.position.x < -2 * width
+      or body.position.y > 3 * height
+      or body.position.y < -2 * height
+end
+
+function merge(body1, body2)
+  local newMass = body1.mass + body2.mass
+
+  body1.velocity.x = (body1.mass * body1.velocity.x + body2.mass * body2.velocity.x) / newMass
+  body1.velocity.y = (body1.mass * body1.velocity.y + body2.mass * body2.velocity.y) / newMass
+
+  if body2.mass > body1.mass then
+    body1.position = body2.position
+  end
+
+  body1.mass = newMass
+  body1.radius = massToRadius(newMass)
+end
+
+function updateAcceleration(body, other)
   local r = dist(
-    Sun.position.x, Sun.position.y,
+    other.position.x, other.position.y,
     body.position.x, body.position.y)
 
-  local a = Sun.mass * GravitationConstant / (r^2)
+  local a = other.mass * GravitationConstant / (r^2)
 
-  local x = (Sun.position.x - body.position.x)
-  local y = (Sun.position.y - body.position.y)
+  local x = (other.position.x - body.position.x)
+  local y = (other.position.y - body.position.y)
 
   local hypotenuse = math.sqrt(x^2 + y^2)
 
-  body.acceleration.x = a * x / hypotenuse
-  body.acceleration.y = a * y / hypotenuse
+  body.acceleration.x = body.acceleration.x + a * x / hypotenuse
+  body.acceleration.y = body.acceleration.y + a * y / hypotenuse
 end
 
 function updateVelocity(dt, body)
@@ -48,16 +107,53 @@ function updatePosition(dt, body)
   body.position.y = body.position.y + dt * body.velocity.y
 end
 
+function resetAcceleration(body)
+  body.acceleration.x = 0
+  body.acceleration.y = 0
+end
+
 function love.update(dt)
-  for _, body in ipairs(Bodies) do
-    updateAcceleration(body)
+  dt = math.min(dt, DtLimit)
+
+  for i, body in pairs(Bodies) do
+    resetAcceleration(body)
+
+    for j, other in pairs(Bodies) do
+      if i ~= j then
+        updateAcceleration(body, other)
+      end
+    end
+
     updateVelocity(dt, body)
     updatePosition(dt, body)
+  end
+
+  local deadBodies = {}
+
+  for i, body in pairs(Bodies) do
+    for j, other in pairs(Bodies) do
+      if j > i then
+        if colliding(body, other) then
+          deadBodies[#deadBodies + 1] = j
+          merge(body, other)
+        end
+      end
+    end
+  end
+
+  for i, body in pairs(Bodies) do
+    if tooFarAway(body) then
+      deadBodies[#deadBodies + 1] = i
+    end
+  end
+
+  for _, i in pairs(deadBodies) do
+    Bodies[i] = nil
   end
 end
 
 function drawBody(body)
-  love.graphics.circle("fill", body.position.x, body.position.y, body.mass^(1/3), 50)
+  love.graphics.circle("fill", body.position.x, body.position.y, body.radius, 50)
 end
 
 function love.draw()
@@ -66,9 +162,13 @@ function love.draw()
     love.graphics.line(Press.x, Press.y, x, y)
   end
 
-  drawBody(Sun)
+  if love.mouse.isDown("r") then
+    local x, y = love.mouse.getPosition()
+    local radius = dist(Press.x, Press.y, x, y)
+    love.graphics.circle("fill", Press.x, Press.y, radius * RadiusFactor, 50)
+  end
 
-  for _, body in ipairs(Bodies) do
+  for _, body in pairs(Bodies) do
     drawBody(body)
   end
 end
@@ -78,11 +178,29 @@ function love.mousepressed(x, y, button)
 end
 
 function love.mousereleased(x, y, button)
-  local xvel, yvel = (x - Press.x) * VelocityFactor, (y - Press.y) * VelocityFactor
+  if button == "r" then
+    local x, y = love.mouse.getPosition()
+    local radius = dist(Press.x, Press.y, x, y) * RadiusFactor
 
-  Bodies[#Bodies + 1] = {
-    position = {x = Press.x, y = Press.y},
-    velocity = {x = xvel, y = yvel},
-    acceleration = {x = 0, y = 0},
-    mass = 25}
+    if radius >= 1 then
+      CreationMass = radiusToMass(radius)
+    end
+  end
+
+  if button == "l" then
+    local xvel, yvel = (x - Press.x) * VelocityFactor, (y - Press.y) * VelocityFactor
+
+    Bodies[#Bodies + 1] = {
+      position = {x = Press.x, y = Press.y},
+      velocity = {x = xvel, y = yvel},
+      acceleration = {x = 0, y = 0},
+      mass = CreationMass,
+      radius = massToRadius(CreationMass)}
+  end
+end
+
+function love.keypressed(key, isrepeat)
+  if key == "g" and not isrepeat then
+    generateBodies()
+  end
 end
